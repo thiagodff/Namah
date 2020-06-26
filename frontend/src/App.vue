a0cad2b96a5bf6e058c3ecec218da337a1d66b73
thiagodff <thiago.fdornelles@gmail.com>
2020-06-25 22:52:06 -0300

    init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Ola Mundo</h1>
+  </div>
+</template>
+
+<script>
+import "./styles/global.css";
+
+export default {
+  name: "App",
+  components: {}
+};
+</script>
+
+<style>
+</style>
