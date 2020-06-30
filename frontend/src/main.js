336d0335a772733a1bd66bbf328b994217a9ecef
thiagodff <thiago.fdornelles@gmail.com>
2020-06-29 23:50:30 -0300

    filter format price

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.filter("formatPrice", (value) => {
+  value = Number(value);
+
+  if (!isNaN(value)) {
+    return value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  } else {
+    return value;
+  }
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
