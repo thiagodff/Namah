6fb1002bed5b91a2320f32a2913658582df75cf2
thiagodff <thiago.fdornelles@gmail.com>
2020-06-29 23:53:23 -0300

    recommendation component

---
+++ frontend/src/components/RecommendProduct.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="product">
+    <img :src="product.img" alt />
+    <h3>{{ product.title }}</h3>
+    <p>{{ product.detail }}</p>
+    <span>{{ product.price | formatPrice }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendProduct",
+  props: ["product"]
+};
+</script>
+
+<style scoped>
+.product {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  font-family: "Robot", sans-serif;
+  margin-left: 25px;
+  overflow-y: auto;
+  overflow: hidden;
+}
+
+.product h3 {
+  font-size: 12px;
+  margin-top: 10px;
+}
+
+.product p {
+  font-size: 11px;
+  color: #666666;
+}
+
+.product span {
+  color: #333333;
+  font-size: 12px;
+  margin-top: 10px;
+}
+</style>
---
+++ frontend/src/components/Recommendation.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="recommendation-wrapper">
+    <div class="recommendation-text">
+      <p>nossos especialisas</p>
+      <strong>recomendam</strong>
+    </div>
+
+    <RecommendProduct :product="book" />
+    <RecommendProduct :product="shirt" />
+    <RecommendProduct :product="carpet" />
+
+    <RecommendProduct :product="book" />
+    <RecommendProduct :product="shirt" />
+    <RecommendProduct :product="carpet" />
+  </div>
+</template>
+
+<script>
+import RecommendProduct from "@/components/RecommendProduct";
+import livroSuaLuz from "@/assets/livroSuaLuz.png";
+import camiseta from "@/assets/camiseta.png";
+import tapete from "@/assets/tapete.png";
+
+export default {
+  name: "Recommendation",
+  components: {
+    RecommendProduct
+  },
+  data() {
+    return {
+      book: {
+        img: livroSuaLuz,
+        title: "Livro Acenda a Sua Luz",
+        detail: "Carol Rache",
+        price: 54
+      },
+      shirt: {
+        img: camiseta,
+        title: "Camiseta Solidária",
+        detail: "Tamanho único",
+        price: 149
+      },
+      carpet: {
+        img: tapete,
+        title: "Tapete Viagem Passáro",
+        detail: "",
+        price: 237
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.recommendation-wrapper {
+  max-width: 1200px;
+  margin: 24px auto;
+  padding: 10px 0;
+  border-top: 1px solid #c4c4c4;
+  border-bottom: 1px solid #c4c4c4;
+
+  display: flex;
+  align-items: center;
+}
+
+@media screen and (max-width: 1400px) {
+  .recommendation-wrapper {
+    margin: 24px 140px;
+  }
+}
+
+.recommendation-text {
+  font-family: "Alice", serif;
+  margin: 0 15px;
+}
+
+.recommendation-text p {
+  font-size: 16.5px;
+  margin-bottom: 4px;
+}
+
+.recommendation-text strong {
+  font-size: 24px;
+}
+</style>
