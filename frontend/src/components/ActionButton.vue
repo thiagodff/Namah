e646b92b68f04d51f57a7f54aac8df728cde7113
thiagodff <thiago.fdornelles@gmail.com>
2020-06-29 23:55:11 -0300

    base components for section

---
+++ frontend/src/components/ActionButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button class="button">
+    <slot></slot>
+    <img :src="arrow" alt="arrow" />
+  </button>
+</template>
+
+<script>
+import arrowRight from "@/assets/arrow-right.svg";
+
+export default {
+  name: "ActionButton",
+  data() {
+    return {
+      arrow: arrowRight
+    };
+  }
+};
+</script>
+
+<style scoped>
+.button {
+  height: 52px;
+  color: #ffffff;
+  background: #73a8a0;
+  border: none;
+  margin-top: 60px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 32px;
+  align-self: center;
+}
+
+.button:hover {
+  background: #6bc0b5;
+}
+
+.button p {
+  font-family: "Robot", sans-serif;
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 10px;
+}
+</style>
---
+++ frontend/src/components/SectionTitle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="section">
+    <div class="stroke"></div>
+    <h2>{{ title }}</h2>
+    <div class="stroke"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionTitle",
+  props: ["title"]
+};
+</script>
+
+<style scoped>
+.section {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.stroke {
+  height: 1px;
+  width: 100%;
+  background: #c4c4c4;
+}
+
+.section h2 {
+  font-family: "Alice", serif;
+  font-size: 46px;
+  margin: 0 10px;
+}
+</style>
