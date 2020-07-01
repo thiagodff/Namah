5a2bee4d0c95ecd6a60b8001a870cde89f17740b
thiagodff <thiago.fdornelles@gmail.com>
2020-06-30 23:31:39 -0300

    shop component

---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="product">
+    <div class="image">
+      <div class="coracao" :class="{ active: isActive }" @click.prevent="isActive = !isActive">
+        <img :src="CoracaoImg" />
+      </div>
+      <img :src="product.img" alt />
+    </div>
+
+    <h4>{{ product.title }}</h4>
+    <p>{{ product.description }}</p>
+    <span>{{ product.price | formatPrice }}</span>
+  </div>
+</template>
+
+<script>
+import CoracaoImg from "@/assets/coracao.svg";
+
+export default {
+  name: "Product",
+  props: ["product"],
+  data() {
+    return {
+      CoracaoImg,
+      isActive: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+.product {
+  cursor: pointer;
+  margin: 0 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.image {
+  display: flex;
+  flex-direction: column;
+}
+
+.coracao {
+  z-index: 10;
+  margin: 0 10px -45px 0;
+  height: 35px;
+  width: 35px;
+  align-self: flex-end;
+  padding: 9px 8px;
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.7);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.active {
+  background-color: #778073;
+}
+
+.product img {
+  width: 100%;
+  height: 100%;
+}
+
+.product h4 {
+  margin-top: 23px;
+  font-size: 18px;
+}
+
+.product p {
+  margin-top: 8px;
+  font-size: 14px;
+  color: #666666;
+}
+
+.product span {
+  margin-top: 23px;
+  font-size: 18px;
+  color: #333333;
+}
+</style>
---
+++ frontend/src/components/Shop.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="shop-wrapper">
+    <SectionTitle title="Namah Shop" />
+
+    <div class="products">
+      <img class="arrow-left" :src="ArrowLeft" alt />
+      <Product v-for="(product, index) in products" :product="product" :key="index" />
+      <img class="arrow-right" :src="ArrowRight" alt />
+    </div>
+
+    <div class="carousel-wrapper">
+      <span v-for="(item, index) in corouselTotal" :class="{active: item.isActive}" :key="index"></span>
+    </div>
+
+    <ActionButton>
+      <p>VEJA TODOS OS PRODUTOS</p>
+    </ActionButton>
+  </div>
+</template>
+
+<script>
+import PascoaImg from "@/assets/pascoa.png";
+import WasabiImg from "@/assets/wasabi.png";
+import ShampooImg from "@/assets/shampoo.png";
+import CondicionadorImg from "@/assets/condicionadorShop.png";
+import ArrowLeft from "@/assets/carrosel-arrow-left.svg";
+import ArrowRight from "@/assets/carrosel-arrow-right.svg";
+
+import SectionTitle from "@/components/SectionTitle";
+import ActionButton from "@/components/ActionButton";
+import Product from "@/components/Product";
+
+export default {
+  name: "Shop",
+  components: {
+    SectionTitle,
+    ActionButton,
+    Product
+  },
+  data() {
+    return {
+      ArrowLeft,
+      ArrowRight,
+      corouselTotal: [
+        {
+          isActive: true
+        },
+        {
+          isActive: false
+        }
+      ],
+      products: [
+        {
+          img: PascoaImg,
+          title: "Ovo de Páscoa de Paçoca",
+          description: "Lorem ipsum dolor",
+          price: 65
+        },
+        {
+          img: WasabiImg,
+          title: "Chantighee com Wasabi 200g",
+          description: "Lorem ipsum dolor",
+          price: 32
+        },
+        {
+          img: ShampooImg,
+          title: "Shampoo Sólido Namah",
+          description: "Lorem ipsum dolor",
+          price: 49
+        },
+        {
+          img: CondicionadorImg,
+          title: "Condicionador Namah",
+          description: "Lorem ipsum dolor",
+          price: 69
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scopedd>
+.shop-wrapper {
+  margin: 120px auto 0 auto;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+}
+
+@media screen and (max-width: 1400px) {
+  .shop-wrapper {
+    margin: 120px 140px 0 140px;
+  }
+}
+
+.products {
+  display: flex;
+  flex-direction: row;
+  margin: 70px -58px 40px -58px;
+}
+
+.arrow-left {
+  margin-right: 28px;
+  cursor: pointer;
+}
+
+.arrow-right {
+  margin-left: 28px;
+  cursor: pointer;
+}
+
+.carousel-wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel-wrapper span {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background: #c4c4c4;
+  margin: 0 5px;
+}
+
+.carousel-wrapper span.active {
+  background: #73a8a0;
+}
+</style>
