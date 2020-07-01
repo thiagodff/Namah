18b676975de78ef5545745edaab18b7d85c81f1e
thiagodff <thiago.fdornelles@gmail.com>
2020-06-30 23:32:20 -0300

    course component

---
+++ frontend/src/components/CourseCard.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="card">
+    <img class="imagem" :src="course.img" alt />
+    <span class="date">{{ course.date}}</span>
+    <h4>{{ course.title }}</h4>
+
+    <div class="description">
+      <div>
+        <p>{{ course.author }}</p>
+        <p>{{ course.duration }}</p>
+      </div>
+
+      <img :src="Arrow" alt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Arrow from "@/assets/arrow-green.svg";
+
+export default {
+  name: "CourseCard",
+  props: ["course"],
+  data() {
+    return {
+      Arrow
+    };
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  margin: 0 12px;
+  background: #ffffff;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+}
+
+.card img.imagem {
+  width: 100%;
+}
+
+.card span {
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 14px;
+  color: #73a8a0;
+}
+
+.card h4 {
+  margin-top: 20px;
+  margin-right: 20px;
+  margin-left: 20px;
+  font-size: 20px;
+}
+
+.description {
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.description p {
+  font-size: 14px;
+  color: #333333;
+}
+
+.description div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ frontend/src/components/Courses.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="courses-wrapper">
+    <SectionTitle title="Cursos" />
+
+    <div class="courses">
+      <CourseCard v-for="(course, index) in courses" :key="index" :course="course" />
+    </div>
+
+    <ActionButton>
+      <p>VEJA TODOS OS CURSOS</p>
+    </ActionButton>
+  </div>
+</template>
+
+<script>
+import YogaImg from "@/assets/yogaCourse.png";
+
+import SectionTitle from "@/components/SectionTitle";
+import ActionButton from "@/components/ActionButton";
+import CourseCard from "@/components/CourseCard";
+
+export default {
+  name: "Courses",
+  components: {
+    SectionTitle,
+    ActionButton,
+    CourseCard
+  },
+  data() {
+    return {
+      courses: [
+        {
+          img: YogaImg,
+          date: "4 Abr - 5 Abr",
+          title: "Imersão Vinyasa Flow",
+          author: "Por Fernando Cunha",
+          duration: "2 dias de curso"
+        },
+        {
+          img: YogaImg,
+          date: "4 Abr - 5 Abr",
+          title: "Imersão Vinyasa Flow",
+          author: "Por Fernando Cunha",
+          duration: "2 dias de curso"
+        },
+        {
+          img: YogaImg,
+          date: "4 Abr - 5 Abr",
+          title: "Imersão Vinyasa Flow",
+          author: "Por Fernando Cunha",
+          duration: "2 dias de curso"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.courses-wrapper {
+  margin: 120px auto 0 auto;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+}
+
+@media screen and (max-width: 1400px) {
+  .courses-wrapper {
+    margin: 120px 140px 0 140px;
+  }
+}
+
+.courses {
+  display: flex;
+  margin: 60px -12px 0 -12px;
+}
+</style>
