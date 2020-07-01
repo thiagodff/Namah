fc5f45bbaaaf3ccad93277d05cce22edfb46d3ef
thiagodff <thiago.fdornelles@gmail.com>
2020-06-30 23:32:47 -0300

    podcast component

---
+++ frontend/src/components/PodcastCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="card">
+    <img class="imagem" :src="podcast.img" alt />
+    <span class="reference">{{ podcast.reference}}</span>
+    <h4>{{ podcast.title }}</h4>
+
+    <div class="description">
+      <p>{{ podcast.description }}</p>
+
+      <img :src="Play" alt />
+    </div>
+  </div>
+</template>
+
+<script>
+import Play from "@/assets/play.svg";
+
+export default {
+  name: "PodcastCard",
+  props: ["podcast"],
+  data() {
+    return {
+      Play
+    };
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  margin: 0 12px;
+  background: #ffffff;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+}
+
+.card img.imagem {
+  width: 100%;
+}
+
+.card span {
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 14px;
+  color: #73a8a0;
+}
+
+.card h4 {
+  margin-top: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 20px;
+}
+
+.description {
+  margin: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.description p {
+  font-size: 14px;
+  margin-right: 15px;
+  color: #333333;
+}
+</style>
---
+++ frontend/src/components/Podcasts.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="podcasts-wrapper">
+    <SectionTitle title="Podcasts" />
+
+    <div class="podcasts">
+      <PodcastCard v-for="(podcast, index) in podcasts" :key="index" :podcast="podcast" />
+    </div>
+
+    <ActionButton>
+      <p>VEJA TODOS OS PODCASTS</p>
+    </ActionButton>
+  </div>
+</template>
+
+<script>
+import Podcast123 from "@/assets/podcast123.png";
+import Podcast122 from "@/assets/podcast122.png";
+import Podcast121 from "@/assets/podcast121.png";
+
+import SectionTitle from "@/components/SectionTitle";
+import ActionButton from "@/components/ActionButton";
+import PodcastCard from "@/components/PodcastCard";
+
+export default {
+  name: "Podcasts",
+  components: {
+    SectionTitle,
+    ActionButton,
+    PodcastCard
+  },
+  data() {
+    return {
+      podcasts: [
+        {
+          img: Podcast123,
+          reference: "Namacast #123",
+          title:
+            "Título do podcast lorem ipsum dolor sit amet, consectetur elit tempor ut labore magna aliqua et dolore",
+          description:
+            "Descrição do episódio do podcast lorem ipsum dolor sit amet, consectetur elit tempor"
+        },
+        {
+          img: Podcast122,
+          reference: "Namacast #122",
+          title:
+            "Título do podcast lorem ipsum dolor sit amet, consectetur elit tempor ut labore magna aliqua et dolore",
+          description:
+            "Descrição do episódio do podcast lorem ipsum dolor sit amet, consectetur elit tempor"
+        },
+        {
+          img: Podcast121,
+          reference: "Namacast #122",
+          title:
+            "Título do podcast lorem ipsum dolor sit amet, consectetur elit tempor ut labore magna aliqua et dolore",
+          description:
+            "Descrição do episódio do podcast lorem ipsum dolor sit amet, consectetur elit tempor"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.podcasts-wrapper {
+  margin: 120px auto 0 auto;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+}
+
+@media screen and (max-width: 1400px) {
+  .podcasts-wrapper {
+    margin: 120px 140px 0 140px;
+  }
+}
+
+.podcasts {
+  display: flex;
+  margin: 60px -12px 0 -12px;
+}
+</style>
