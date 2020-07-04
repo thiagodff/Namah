e443dfbd74237c1d682b2f555c401d7e5c039d88
thiagodff <thiago.fdornelles@gmail.com>
2020-07-04 15:15:17 -0300

    concepts section responsive

--- frontend/src/components/Concepts.vue
+++ frontend/src/components/Concepts.vue
@@ -48,12 +48,6 @@ export default {
   flex-direction: column;
 }
 
-@media screen and (max-width: 1400px) {
-  .concept-wrapper {
-    margin: 120px 140px 0 140px;
-  }
-}
-
 .concepts {
   display: flex;
   margin: 60px -12px 0 -12px;
@@ -65,5 +59,56 @@ export default {
 
 .concept img {
   width: 100%;
+}
+
+@media screen and (max-width: 1400px) {
+  .concept-wrapper {
+    margin: 120px 140px 0 140px;
+  }
+}
+
+@media screen and (max-width: 1230px) {
+  .concept-wrapper {
+    margin: 120px 60px 0 60px;
+  }
+}
+
+@media screen and (max-width: 1080px) {
+  .concepts {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+
+@media screen and (max-width: 790px) {
+  .concept-wrapper {
+    margin: 60px 15px 0 15px;
+  }
+
+  .concepts {
+    margin-top: 24px;
+  }
+
+  .concept {
+    margin-top: 12px;
+  }
+}
+
+@media screen and (max-width: 635px) {
+  .concept {
+    flex: 1 0 130px;
+  }
+}
+
+@media screen and (max-width: 485px) {
+  .concept {
+    flex: 1 0 95px;
+  }
+}
+
+@media screen and (max-width: 380px) {
+  .concept {
+    flex: 1 0 80px;
+  }
 }
 </style>
