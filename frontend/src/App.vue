e246ff40868425edda93e8077092461e739175d5
thiagodff <thiago.fdornelles@gmail.com>
2020-07-04 15:06:49 -0300

    offset header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
     <Header />
-    <ul>
-      <li>ola</li>
-    </ul>
+    <FixedButtons />
+
+    <Banner />
+
+    <Highlights />
+    <Recommendation />
+
+    <div class="offset" id="blog"></div>
+    <Blog />
+
+    <div class="offset" id="shop"></div>
+    <Shop />
+
+    <div class="offset" id="courses"></div>
+    <Courses />
+
+    <div class="offset" id="podcasts"></div>
+    <Podcasts />
+    <Concepts />
+
+    <div class="offset" id="contact"></div>
+    <Footer />
   </div>
 </template>
 
@@ -11,14 +30,58 @@
 import "./styles/global.css";
 
 import Header from "@/components/Header";
+import FixedButtons from "@/components/FixedButtons";
+import Banner from "@/components/Banner";
+import Highlights from "@/components/Highlights";
+import Recommendation from "@/components/Recommendation";
+import Blog from "@/components/Blog";
+import Shop from "@/components/Shop";
+import Courses from "@/components/Courses";
+import Podcasts from "@/components/Podcasts";
+import Concepts from "@/components/Concepts";
+import Footer from "@/components/Footer";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    FixedButtons,
+    Banner,
+    Highlights,
+    Recommendation,
+    Blog,
+    Shop,
+    Courses,
+    Podcasts,
+    Concepts,
+    Footer
   }
 };
 </script>
 
 <style>
+#app {
+  margin-top: 160px;
+}
+
+.offset:before {
+  display: block;
+  content: " ";
+  width: 100%;
+  height: 10px;
+  margin-top: -10px;
+  visibility: hidden;
+}
+
+@media screen and (max-width: 790px) {
+  #app {
+    margin-top: 124px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  #app {
+    margin-top: 94px;
+  }
+}
 </style>
