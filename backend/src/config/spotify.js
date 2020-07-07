2fa44cb71c8f505a7cd0c05a011a952d9a70ec87
thiagodff <thiago.fdornelles@gmail.com>
2020-07-07 11:22:15 -0300

    frontend deploy

--- backend/src/config/spotify.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  token: process.env.SPOTIFY_TOKEN,
-  playlist: {
-    IDRock: "37i9dQZF1DWXRqgorJj26U",
-    IDParty: "37i9dQZF1DX1oTjVZnUYT3",
-    IDPop: "37i9dQZF1DX1ngEVM0lKrb",
-    IDClassic: "37i9dQZF1DWWEJlAGA9gs0",
-  },
-};
--- backend/src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const WeatherController = require("./app/controllers/WeatherController");
-
-const routes = express.Router();
-
-routes.post("/weather", WeatherController.create);
-
-module.exports = routes;
